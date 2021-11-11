a56f6be16dc72dd5b594f9eacde535875c6793c9
palemtech <94140016+palemtech@users.noreply.github.com>
2021-11-11 18:02:26 -0500

    updating js

--- js/app-builder-menus.js
+++ js/app-builder-menus.js
@@ -62,7 +62,7 @@ function showChapterMenu(chs, hasIntro, baseRef) {
     popUp.style.top = rect.top + rect.height + scrollTop + "px";
     popUp.style.left = "12.5%";
 	popUp.style.right = "12.5%";
-    popUp.style.width = 70%;//200 + "px";
+    popUp.style.width = "70%";//200 + "px";
     popUp.style.maxHeight = 400 + "px";
      
 	baseText = "";
