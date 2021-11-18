26694c79c283195af75014dbe4ff1d759e15c38d
palemtech <94140016+palemtech@users.noreply.github.com>
2021-11-18 10:27:52 -0500

    updated css

--- pwa-main.js
+++ pwa-main.js
@@ -1,6 +1,41 @@
+document.addEventListener('message', messageEvent => {
+  if (messageEvent.data === 'skipWaiting') return skipWaiting();
+});
+
+function listenForWaitingServiceWorker(reg, callback) {
+  function awaitStateChange() {
+    reg.installing.addEventListener('statechange', function() {
+      if (this.state === 'installed') callback(reg);
+    });
+  }
+  if (!reg) return;
+  if (reg.waiting) return callback(reg);
+  if (reg.installing) awaitStateChange();
+  reg.addEventListener('updatefound', awaitStateChange);
+}
+
+// reload once when the new Service Worker starts activating
+var refreshing;
+navigator.serviceWorker.addEventListener('controllerchange',
+  function() {
+    if (refreshing) return;
+    refreshing = true;
+    window.location.reload();
+  }
+);
+
+function promptUserToRefresh(reg) {
+  // this is just an example
+  // don't use window.confirm in real life; it's terrible
+  if (window.confirm("Persi baru tersedia. Klik Ok óntók ngupdate?")) {
+    reg.waiting.postMessage('skipWaiting');
+  }
+}
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('./sw.js').then(function(registration) {
+		listenForWaitingServiceWorker(registration, promptUserToRefresh);
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
